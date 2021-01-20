c989f918b51d81a296ac060cc15917f17d918186
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2021-01-20 21:27:01 +0530

    making emails public

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,15 +1,4 @@
-import getCurrentUserId from '../utils/getCurrentUserId';
-
 const User = {
-  async email(parent, args, { prisma, request }) {
-    const userId = await getCurrentUserId(request, false);
-
-    if (userId && parent.id === userId) {
-      return parent.email;
-    } else {
-      return null;
-    }
-  },
   posts(parent, args, { prisma }) {
     return prisma.post.findMany({
       where: {
